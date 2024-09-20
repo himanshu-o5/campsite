d04292ce1bc47e2b2e4a941396234f725d77f19a
himanshu-o5 <himanshushukla71.9@gmail.com>
2024-09-20 13:24:04 +0530

    Added : Form with Local Storage

---
+++ form/add.js
@@ -0,0 +1,37 @@
+const submitButton = document.getElementById('submitButton');
+
+submitButton.addEventListener("click", ()=>{
+    // get all input elements
+    const campName = document.getElementById('camp-name');
+    const campImage = document.getElementById('camp-image-url');
+    const campContinent = document.getElementById('camp-continent');
+
+    // Check all inputs are there
+    if(!campName.value || !campImage.value || campContinent.value == 0){
+        location.reload();
+    }
+
+    // Add to local storage
+    const newCamp = {
+        name: campName.value,
+        imageURL: campImage.value
+    };    
+
+    // Get respective continent object
+    const oldContinentString = window.localStorage.getItem(campContinent.value);
+    const oldContinents = JSON.parse(oldContinentString);
+
+    // Add it to new array
+    let newContinents = [];
+    if(oldContinents == null){
+        newContinents = [newCamp];
+    }
+    else{
+        newContinents = [...oldContinents, newCamp];
+    }
+
+    // Set new array to local storage
+    window.localStorage.setItem(campContinent.value, JSON.stringify(newContinents));
+
+
+});
