52bc13c09416dc8944d04a7fde291504c231e3e8
himanshu-o5 <himanshushukla71.9@gmail.com>
2024-09-29 20:19:01 +0530

    Added : Seeds and Readme Assets

---
+++ seed.js
@@ -0,0 +1,157 @@
+const asia = [
+    {
+        name: "Petronas Twin Tower, Malaysia",
+        imageURL: "https://study.com/cimages/multimages/16/petronas_twin_towers_night9144876942217746902.jpeg"
+    },
+    {
+        name: "Big Buddha Phuket, Thailand",
+        imageURL: "https://d2rdhxfof4qmbb.cloudfront.net/wp-content/uploads/2024/02/Big-Buddha_cover.jpg"
+    },
+    {
+        name: "Taj Mahal, India",
+        imageURL: "https://images.unsplash.com/photo-1564507592333-c60657eea523?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        name: "Amber Palace, India", 
+        imageURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTc7kJdn2rJQ3PHZV2IrQMeHlfVyquP0ySkcg&s"
+    },
+    {
+        name: "The Bund, China",
+        imageURL: "https://darcawards.com/wp-content/uploads/2020/08/Shanghai-Bund-3.jpg"
+    },
+    {
+        name: "Kiyomizu-dera Temple, Japan",
+        imageURL: "https://imagedelivery.net/Ahwf2rDz3ZiXm1qkpDKg1w/live/-/media/A14CEC794AC0471EA6702B273CBF9FD9.jpg/w=750?d=20240709T024707&w=750"
+    },
+    {
+        name: "The Great Wall of China, China",
+        imageURL: "https://cdn.britannica.com/82/94382-050-20CF23DB/Great-Wall-of-China-Beijing.jpg"
+    }
+];
+
+const europe = [
+    {
+        name: "Venice, Italy",
+        imageURL: "https://lp-cms-production.imgix.net/2021-06/GettyRF_543346423.jpg?w=600&h=400"
+    },
+    {
+        name: "The Colosseum, Rome",
+        imageURL: "https://cdn.mos.cms.futurecdn.net/BiNbcY5fXy9Lra47jqHKGK.jpg"
+    },
+    {
+        name: "Ring of Kerry, Ireland",
+        imageURL: "https://lp-cms-production.imgix.net/2023-02/GettyImages-1313352819.jpg?fit=crop&w=1200&auto=format&q=75"
+    },
+    {
+        name: "Eiffel Tower, Paris",
+        imageURL: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/29/eb/ff/33/vue-sur-la-tour-euiffel.jpg?w=1200&h=-1&s=1"
+    },
+    {
+        name: "Basílica de la Sagrada Familia, Barcelona",
+        imageURL: "https://cdn.britannica.com/15/194815-050-08B5E7D1/Nativity-facade-Sagrada-Familia-cathedral-Barcelona-Spain.jpg"
+    },
+    {
+        name: "The British Museum, India",
+        imageURL: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0e/ef/2d/8e/photo1jpg.jpg?w=1200&h=-1&s=1"
+    },
+    {
+        name: "Stonehenge, England",
+        imageURL: "https://upload.wikimedia.org/wikipedia/commons/3/3c/Stonehenge2007_07_30.jpg"
+    }
+]
+
+const africa = [
+    {
+        name: "Victoria Falls, Zimbabwe",
+        imageURL: "https://i.natgeofe.com/n/177faece-da85-4458-ad91-3b83a89af602/92754.jpg"
+    },
+    {
+        name: "Malawi",
+        imageURL: "https://diplomatist.com/wp-content/uploads/2022/07/featured-image.jpg"
+    },
+    {
+        name: "Sahara Desert",
+        imageURL: "https://media.istockphoto.com/id/665139550/photo/morocco-sand-dunes-of-sahara-desert.jpg?s=612x612&w=0&k=20&c=lg53jUtnF9wvknqVpizoxGniEuwkchg0M_bJAMO50Xk="
+    },
+    {
+        name: "Serengeti National Park, Tanzania",
+        imageURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTEfQF8HJPZ1vrkoPZJTcEQVFRvHbuJzFjVg&s"
+    },
+    {
+        name: "Madagascar, Africa",
+        imageURL: "https://i.natgeofe.com/k/e8001682-cf16-40d8-a0ab-3542a1415424/madagascar-antananarivo_square.jpg"
+    }
+]
+
+const america = [
+    {
+        name: "Niagra Falls",
+        imageURL: "https://media.timeout.com/images/103674438/1024/576/image.webp"
+    },
+    {
+        name: "Statue of Liberty, New York",
+        imageURL: "https://media.timeout.com/images/103897464/1024/576/image.webp"
+    },
+    {
+        name: "Mt Rushmore, Keystone",
+        imageURL: "https://media.timeout.com/images/106157364/1024/576/image.webp"
+    },
+    {
+        name: "The Strip, Las Vegas",
+        imageURL: "https://media.timeout.com/images/101515225/1024/576/image.webp"
+    },
+    {
+        name: "The Grand Canyon, Arizona",
+        imageURL: "https://media.timeout.com/images/102908703/1024/576/image.webp"
+    },
+    {
+        name: "Golden Gate Bridge, San Francisco",
+        imageURL: "https://media.timeout.com/images/106034139/1024/576/image.webp"
+    },
+    {
+        name: "Hoover Dam, Nevada",
+        imageURL: "https://media.timeout.com/images/105795060/1024/576/image.webp"
+    },
+    {
+        name: "Little Havana, Miami",
+        imageURL: "https://media.timeout.com/images/105686990/1024/576/image.webp"
+    }
+]
+
+
+const australia = [
+    {
+        name: "Opera House, Sydney",
+        imageURL: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/09/cf/69/07/sydney-harbour.jpg?w=1200&h=-1&s=1"
+    },
+    {
+        name: "Manly Beach, Sydney",
+        imageURL: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/08/5e/31/a3/manly-beach.jpg?w=900&h=-1&s=1"
+    },
+    {
+        name: "City Circle Tram, Melbourne",
+        imageURL: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/07/db/d8/a3/perfecto.jpg?w=1200&h=-1&s=1"
+    },
+    {
+        name: "Uluru",
+        imageURL: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/06/45/d6/d0/uluru.jpg?w=1200&h=-1&s=1"
+    },
+    {
+        name: "Bondi to Coogee Walk",
+        imageURL: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/06/a3/74/63/bondi-to-coogee-beach.jpg?w=1200&h=-1&s=1"
+    }
+]
+
+
+
+const asiaLocalStorage = JSON.parse(window.localStorage.getItem("asia"));
+const europeLocalStorage = JSON.parse(window.localStorage.getItem("europe"));
+const africaLocalStorage = JSON.parse(window.localStorage.getItem("africa"));
+const americaLocalStorage = JSON.parse(window.localStorage.getItem("america"));
+const australiaLocalStorage = JSON.parse(window.localStorage.getItem("australia"));
+
+if(!asiaLocalStorage) window.localStorage.setItem("asia", JSON.stringify(asia));
+if(!europeLocalStorage) window.localStorage.setItem("europe", JSON.stringify(europe));
+if(!africaLocalStorage) window.localStorage.setItem("africa", JSON.stringify(africa));
+if(!americaLocalStorage) window.localStorage.setItem("america", JSON.stringify(america));
+if(!australiaLocalStorage) window.localStorage.setItem("australia", JSON.stringify(australia));
